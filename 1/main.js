afae58d2229279c7f3477b622c77caa615a70e58
Viktorshcherbyna <viktorshcherbyna1705@gmail.com>
2019-06-23 12:08:26 +0300

    task1

--- 1/main.js
+++ 1/main.js
@@ -1,1 +1,39 @@
-alert(' its work');
+var user = ['vasya', 'petya', 'anya', 'vera', 'alex'];
+var animal = ['lev', 'kot', 'loshad', 'los', 'skyns'];
+var drinks = ['tea', 'coffee', 'mohito', 'sprite'];
+
+function renderListUser(first, list) {
+    var block = document.querySelector('#list');
+    var html = '';
+
+    for (var names of user) {
+        html += `<li onclick = alert(this.innerHTML)>${names}</li>`;
+    }
+    block.innerHTML = html;
+}
+
+function renderListAnimal(second, anim) {
+    var block = document.querySelector('#anim');
+    var html = '';
+
+    for (var animals of animal) {
+        html += `<li onclick = alert(this.innerHTML)>${animals}</li>`;
+    }
+    block.innerHTML = html;
+}
+
+function renderListDrink(third, cold) {
+    var block = document.querySelector('#cold');
+    var html = '';
+
+    for (var drunk of drinks) {
+        html += `<li onclick = alert(this.innerHTML)>${drunk}</li>`;
+    }
+    block.innerHTML = html;
+
+}
+
+
+renderListUser('first, #list');
+renderListAnimal('second, #anim');
+renderListDrink('third, #cold');
