31fb279bb345ca705e5db12d1bbf0d6ce6c9aa74
Viktorshcherbyna <viktorshcherbyna1705@gmail.com>
2019-06-28 14:16:23 +0300

    create task 3

--- 3/main.js
+++ 3/main.js
@@ -1,1 +1,26 @@
-alert(' its work');
+document.querySelector('#c').style.width = "300px";
+document.querySelector('#c').style.height = "300px";
+document.querySelector('#c').style.border = "2px solid black";
+
+
+var currentColor = 0;
+var colors = ['red', 'blue', 'green', 'black', 'yellow', 'darkgrey', 'maroon', 'orange'];
+
+function colPrev() {
+    currentColor--;
+    if (currentColor < 0) currentColor = 7;
+    var a = document.getElementById("c");
+    a.style.backgroundColor = colors[currentColor];
+}
+
+var currentColor2 = 0;
+
+var currentColor2 = 0;
+
+
+function colNext() {
+    currentColor++;
+    if (currentColor > 7) currentColor = 0;
+    var a = document.getElementById("c");
+    a.style.backgroundColor = colors[currentColor];
+}
